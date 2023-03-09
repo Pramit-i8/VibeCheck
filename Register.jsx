b04e3245bad47002add6b7045adb00dbb02fa8d4
Pramit-i8 <pramitdey22@gmail.com>
2023-03-09 09:00:01 -0500

    registed commented

--- Register.jsx
+++ Register.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 export const Register = (props) => {
+    //Each of these state variables is initialized to an empty string.
+    //useState hook returns the current value of the state variable and calls the function to update the state variable 
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [name, setName] = useState('');
@@ -9,11 +11,14 @@ export const Register = (props) => {
         e.preventDefault();
         console.log(email);
     }
-
+    //handle the submission of a form, by stopping the refresh of the form submission 
+    //and then logging data to the console to update state variables
+    
     return (
         <div className="auth-form-container">
             <h2>Register</h2>
         <form className="register-form" onSubmit={handleSubmit}>
+            //renders an HTML label and an input element that together create a form field
             <label htmlFor="name">Full name</label>
             <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
             <label htmlFor="email">Email</label>
@@ -22,7 +27,7 @@ export const Register = (props) => {
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
             <button type="submit">Log In</button>
         </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button> 
     </div>
     )
 }
